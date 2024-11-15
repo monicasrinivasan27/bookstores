30c4d3a2cfa510c0ef774a8d904f6937e9722f91
monicasrinivasan27 <srinivasan.monica@gmail.com>
2024-11-15 14:09:41 -0600

    Test

---
+++ src/main/java/custom/application/bookstores/controllers/bookStoreCrud.java
@@ -0,0 +1,10 @@
+package custom.application.bookstores.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Arrays;
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:3000")
+public class bookStoreCrud {
+
+}
